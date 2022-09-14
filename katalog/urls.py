851c5a736320cbd7a5fd511ed8924a5544b70049
Fabian <fabiandion12@gmail.com>
2022-09-14 11:22:52 +0700

    Add Tugas 2 PBP

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,9 @@
 # TODO: Implement Routings Here
+from django.urls import path
+from katalog.views import show_items
+
+app_name = 'katalog'
+
+urlpatterns = [
+    path('', show_items, name='show_items'),
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+
+def show_items(request):
+    response = {
+        'list_item': CatalogItem.objects.all(),
+        'name': "Fabian Akmal Arkandion",
+        'npm': "2106750660"
+    }
+    return render(request, 'katalog.html', response)
